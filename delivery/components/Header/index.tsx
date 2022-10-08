1d11b44263fe552cc5b1098e312c032014303260
unknown <igormedeiros.f@gmail.com>
2022-10-08 17:34:57 -0300

    iniciando componente header e pagina de login

---
+++ delivery/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.css'
+import BackIcon from './backIcon.svg'
+
+export const Header = () => {
+    return (
+        <div className={styles.container}>
+            ...
+        </div>
+    )
+}
---
+++ delivery/pages/[tenant]/login.tsx
@@ -0,0 +1,62 @@
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+import { useEffect } from 'react'
+import { Header } from '../../components/Header'
+import { useAppContext } from '../../contexts/AppContext'
+import { useApi } from '../../libs/useApi'
+
+import styles from '../../styles/Home.module.css'
+import { Tenant } from '../../types/Tenant'
+
+const Login = (data: Props) => {
+    const { tenant, setTenant } = useAppContext()
+
+    useEffect(() => {
+        setTenant(data.tenant)
+    }, [])
+
+    const handleSearch = (searchValue: string) => {
+        console.log(`Você está digitando ${searchValue}`)
+    }
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Login | {data.tenant.name}</title>
+            </Head>
+          
+          <Header/>
+        </div>
+    )
+}
+
+export default Login
+
+type Props = {
+    tenant: Tenant
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { tenant: tenantSlug } = context.query
+
+    const api = useApi()
+
+    // get tenant
+    const tenant = await api.getTenant(tenantSlug as string)
+
+    if (!tenant) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
+    return {
+        props: {
+            tenant
+        }
+    }
+}
