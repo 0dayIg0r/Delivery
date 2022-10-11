46f1d52e267f79899fb4016230ece82c4d1a2fdb
unknown <igormedeiros.f@gmail.com>
2022-10-10 23:34:18 -0300

    tela do produto

--- delivery/pages/[tenant]/product/[id].tsx
+++ delivery/pages/[tenant]/product/[id].tsx
@@ -1,80 +1,110 @@
 import { GetServerSideProps, } from 'next'
 import Head from 'next/head'
 import { useEffect } from 'react'
+import { Button } from '../../../components/Button'
 import { Header } from '../../../components/Header'
 import { useAppContext } from '../../../contexts/AppContext'
 import { useApi } from '../../../libs/useApi'
+import { useFormatter } from '../../../libs/useFormater'
 import styles from '../../../styles/Product-id.module.css'
 import { Product } from '../../../types/Products'
 import { Tenant } from '../../../types/Tenant'
 
 const Product = (data: Props) => {
-  const { tenant, setTenant } = useAppContext()
+    const { tenant, setTenant } = useAppContext()
 
-  useEffect(() => {
-    setTenant(data.tenant)
-  }, [])
+    useEffect(() => {
+        setTenant(data.tenant)
+    }, [])
 
+    const formatter = useFormatter()
 
-  return (
-    <div className={styles.container}>
-        <Head>
-            <title>{data.product.name} | {data.tenant.name}</title>
-        </Head>
+    const handleAddToCart = () => {
 
-        <div className={styles.headerArea}>
-            <Header
-            color={data.tenant.mainColor}
-            backHref={`/${data.tenant.slug}`}
-            title='Produto'
-            invert
-            />
+    }
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>{data.product.name} | {data.tenant.name}</title>
+            </Head>
+
+            <div className={styles.headerArea}>
+                <Header
+                    color={data.tenant.mainColor}
+                    backHref={`/${data.tenant.slug}`}
+                    title='Produto'
+                    invert
+                />
+            </div>
+
+            <div className={styles.headerBg} style={{ backgroundColor: data.tenant.mainColor }}>
+
+            </div>
+
+            <div className={styles.productImage}>
+                <img src={data.product.image} alt="" />
+            </div>
+            <div className={styles.category}>{data.product.categoryName}</div>
+            <div className={styles.title} style={{ borderBottomColor: data.tenant.mainColor }}>{data.product.name}</div>
+            <div className={styles.line} ></div>
+
+            <div className={styles.description}>{data.product.description}</div>
+
+            <div className={styles.qtText}>Quantidade</div>
+            <div className={styles.area}>
+                <div className={styles.areaLeft}>...</div>
+                <div
+                    className={styles.areaRight}
+                    style={{ color: data.tenant.mainColor }}>
+                    {formatter.formatPrice(data.product.price)}
+                </div>
+            </div>
+
+            <div className={styles.buttonArea}>
+                <Button
+                    color={data.tenant.mainColor}
+                    label='Adicionar à sacola'
+                    onClick={handleAddToCart}
+                    fill
+                />
+            </div>
         </div>
-
-        <div className={styles.headerBg} style={{backgroundColor: data.tenant.mainColor}}>
-
-        </div>
-
-        <div className={styles.productImage}>
-            <img src={data.product.image} alt="" />
-        </div>
-        ...
-    </div>
-  )
+    )
 }
 
 export default Product
 
 type Props = {
-  tenant: Tenant,
-  product: Product
+    tenant: Tenant,
+    product: Product
 }
 
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { tenant: tenantSlug, id } = context.query
+    const { tenant: tenantSlug, id } = context.query
 
-  const api = useApi(tenantSlug as string)
+    const api = useApi(tenantSlug as string)
 
-  // get tenant
-  const tenant = await api.getTenant()
+    // get tenant
+    const tenant = await api.getTenant()
 
-  if (!tenant) {
+    if (!tenant) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
+    // get products
+    const product = await api.getProduct(id as string)
+
     return {
-      redirect: {
-        destination: '/',
-        permanent: false
-      }
+        props: {
+            tenant,
+            product
+        }
     }
-  }
-
-  // get products
-  const product = await api.getProduct(id as string)
-
-  return {
-    props: {
-      tenant,
-      product
-    }
-  }
 }
